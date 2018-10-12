6931e5ce717b3b1e6492801632cbc22fc7a59716
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-12 20:53:41 +0100

    valid word square

---
+++ ValidWordSquare/ValidWordSquareCls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ValidWordSquare
+{
+    public class ValidWordSquareCls
+    {
+        public bool ValidWordSquare(IList<string> words)
+        {
+            var matrix = new char[words.Count][];
+
+            for (var i = 0; i < words.Count; i++)
+            {
+                var wordCharArr = words[i].ToCharArray();
+
+                matrix[i] = wordCharArr;
+            }
+
+            for (var i = 0; i < matrix.Length; i++)
+            for (var j = 0; j < matrix[i].Length; j++)
+                if (j >= matrix.Length || i >= matrix[j].Length || !matrix[i][j].Equals(matrix[j][i]))
+                    return false;
+
+            return true;
+        }
+    }
+}
