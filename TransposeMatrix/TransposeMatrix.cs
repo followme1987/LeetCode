b3995f59bde54633a82fbec07cc977ef1f7cd4fb
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-13 23:26:25 +0100

    transpose matrix

---
+++ TransposeMatrix/TransposeMatrix.cs
@@ -0,0 +1,27 @@
+﻿namespace TransposeMatrix
+{
+    public class TransposeMatrix
+    {
+        public int[][] Transpose(int[][] A)
+        {
+            var count = A[0].Length;
+
+            var result = new int[count][];
+
+            for (var i = 0; i < count; i++)
+            {
+                result[i] = new int[A.Length];
+
+                var newRow = new int[A.Length];
+
+                for (var j = 0; j < A.Length; j++)
+                    //better performance than result[i][j] = A[j][i]
+                    newRow[j] = A[j][i];
+
+                result[i] = newRow;
+            }
+
+            return result;
+        }
+    }
+}
