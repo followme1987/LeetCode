2ca99e18b83419e94619526307d8705da182a69c
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-14 14:20:29 +0100

    reverseString

---
+++ ReverseString/ReverseStringCls.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseString
+{
+    public class ReverseStringCls
+    {
+        public string ReverseString(string s)
+        {
+            var charArr = s.ToCharArray();
+            var resultArr = new char[charArr.Length];
+            var index = 0;
+            for (var i = charArr.Length - 1; i >= 0; i--)
+            {
+                resultArr[index] = charArr[i];
+
+                index++;
+            }
+
+            return new string(resultArr);
+        }
+    }
+}
