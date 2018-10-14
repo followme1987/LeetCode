54e3bb79f9344e9b839199754163be28eb9c4d72
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-14 15:42:17 +0100

    commit

---
+++ ReverseWordsInAStringIII/ReverseWordsInAStringIII.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ReverseWordsInAStringIII
+{
+    public class ReverseWordsInAStringIII
+    {
+        public string ReverseWords(string s)
+        {
+            var subString = s.Split(' ');
+            var resultStr = new StringBuilder();
+
+            foreach (var str in subString)
+            {
+                var charArr = str.ToCharArray();
+
+                var resultArr = new char[charArr.Length];
+
+                var index = 0;
+
+                for (var i = charArr.Length - 1; i >= 0; i--)
+                {
+                    resultArr[index] = charArr[i];
+                    index++;
+                }
+
+                resultStr = resultStr.Append(new string(resultArr) + " ");
+            }
+
+            return resultStr.ToString().Trim();
+        }
+    }
+}
