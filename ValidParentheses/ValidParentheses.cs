56aaf65425a5479a991c138a9e87d8036f522e8c
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-16 20:33:30 +0100

    commit

--- ValidParentheses/ValidParentheses.cs
+++ ValidParentheses/ValidParentheses.cs
@@ -1,58 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ValidParentheses
 {
     public class ValidParentheses
     {
-        int resultValue = 0;
-
         public bool IsValid(string s)
         {
-            //"[([]])"
+            var stack = new Stack<int>();
             var dic = new Dictionary<char, int> {{'(', 1}, {'{', 2}, {'[', 3}, {')', -1}, {'}', -2}, {']', -3}};
+
+            if (s.Equals(""))
+                return true;
 
             var charArr = s.ToCharArray();
 
-            if (charArr.Length > 0 && dic[charArr[0]] < 0)
-                return false;
-
-           
             for (var i = 0; i < charArr.Length; i++)
-            {
-                resultValue += dic[charArr[i]];
-                
-                if (resultValue < 0)
-                    return false;
-
-                if (i > 0)
+                if (dic[charArr[i]] > 0)
                 {
-                    //if (charArr[i] < 0 && charArr[i - 1] > 0 && charArr[i] + charArr[i - 1] == 0)
-                    //{
-                    //    var removeIndex = i - 1;
-                    //    s = s.Remove(removeIndex).Remove(i);
-
-                    //    IsValid(s);
-                    //}
-
-                    if (dic[charArr[i]] < 0 && dic[charArr[i - 1]] > 0 && dic[charArr[i]] + dic[charArr[i - 1]] != 0)
-                    {
-                        return false;
-                    }
-                    else if(dic[charArr[i]] < 0 && dic[charArr[i - 1]] > 0 && dic[charArr[i]] + dic[charArr[i - 1]] == 0)
-                    {
-                        var removeIndex = i - 1;
-                        s = s.Remove(removeIndex).Remove(i);
-
-                        IsValid(s);
-                    }
-
-                   
+                    stack.Push(dic[charArr[i]]);
                 }
-              
-            }
+                else
+                {
+                    if (stack.Count == 0 || dic[charArr[i]] + stack.Pop() != 0)
+                        return false;
+                }
 
-            return resultValue == 0;
+            return stack.Count == 0;
         }
     }
 }
