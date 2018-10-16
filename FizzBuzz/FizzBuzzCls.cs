23371e7849f30fe8e5a50f074b2fead55e4a8150
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-16 20:09:20 +0100

    tidy up the code

--- FizzBuzz/FizzBuzzCls.cs
+++ FizzBuzz/FizzBuzzCls.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace FizzBuzz
 {
@@ -8,28 +6,19 @@ namespace FizzBuzz
     {
         public IList<string> FizzBuzz(int n)
         {
-
             var result = new List<string>();
 
             while (n > 0)
             {
                 if (n % 3 == 0 && n % 5 == 0)
-                {
                     result.Add("FizzBuzz");
-                }
                 else if (n % 3 == 0)
-                {
                     result.Add("Fizz");
-                }
 
                 else if (n % 5 == 0)
-                {
                     result.Add("Buzz");
-                }
                 else
-                {
                     result.Add(n.ToString());
-                }
 
                 n--;
             }
