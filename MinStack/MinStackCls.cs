3530e671ba8b78f339332ae9783bc7034765e42e
ywu <yichen.wu@paddypowerbetfair.com>
2018-10-24 18:17:55 +0100

    commit

---
+++ MinStack/MinStackCls.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MinStack
+{
+    public class MinStackCls
+    {
+
+        private int[] _stackArray;
+
+        private int writeIndex = 0;
+        /** initialize your data structure here. */
+        public MinStackCls()
+        {
+            _stackArray = new int[10];
+        }
+
+        public void Push(int x)
+        {
+
+
+            if (writeIndex == _stackArray.Length)
+            {
+                var extended = new int[_stackArray.Length + _stackArray.Length / 2];
+                Array.Copy(_stackArray, extended, _stackArray.Length);
+                _stackArray = extended;
+            }
+            _stackArray[writeIndex] = x;
+            writeIndex++;
+
+        }
+
+        public void Pop()
+        {
+            if (writeIndex > 0)
+                writeIndex--;
+        }
+
+        public int Top()
+        {
+            if (writeIndex > 0)
+                return _stackArray[writeIndex - 1];
+            return _stackArray[0];
+        }
+
+        public int GetMin()
+        {
+            if (writeIndex == 0)
+                return _stackArray[0];
+
+            var min = int.MaxValue;
+            for (var i = 0; i <= writeIndex - 1; i++)
+            {
+                min = Math.Min(_stackArray[i], min);
+
+            }
+
+            return min;
+        }
+    }
+}
---
+++ MinStackTest/MinStackClsTest.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+
+namespace MinStackTest
+{
+    public class MinStackClsTest
+    {
+        [Fact]
+        public void MinStack_Test()
+        {
+            var stack = new MinStack.MinStackCls();
+            stack.Push(-2);
+            stack.Push(0);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);
+            stack.Push(-3);           
+            stack.Pop();
+           var result = stack.Top(); 
+           var result2 = stack.GetMin();
+
+            Assert.Equal(-3,result);
+            Assert.Equal(-3,result2);
+        }
+    }
+}
