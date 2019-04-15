4e2e79f459508882b39466b103c0b00fd6c48116
Yichen Wu <followme_1987@hotmail.com>
2019-04-15 22:13:00 +0100

    tree test

---
+++ BinaryTreeLevelOrderTraversalII/BinaryTreeLevelOrderTraversalIICls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreeLevelOrderTraversalII
+{
+    public class BinaryTreeLevelOrderTraversalIICls
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            if (root == null)
+                return new List<IList<int>>();
+
+            var q = new Queue<TreeNode>();
+
+            var result = new List<IList<int>>();
+
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                var level = new List<int>();
+
+                var queueSize = q.Count;
+
+                for(var i = 0; i < queueSize; i++)
+                {
+                    var n = q.Dequeue();
+
+                    level.Add(n.val);
+
+                    if (n.left != null)
+                        q.Enqueue(n.left);
+                    if (n.right != null)
+                        q.Enqueue(n.right);
+                }
+
+                result.Add(level);
+            }
+
+            result.Reverse();
+
+            return result;
+        }
+    }
+}
---
+++ N-aryTreeLevelOrderTraversal/N_aryTreeLevelOrderTraversalCls.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace N_aryTreeLevelOrderTraversal
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+    public class N_aryTreeLevelOrderTraversalCls
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            if (root == null)
+                return new List<IList<int>>();
+
+            var resultList = new List<IList<int>>();
+            var queue = new Queue<Node>();
+
+            queue.Enqueue(root);
+
+            while(queue.Count > 0)
+            {
+                var list = new List<int>();
+
+                var queueSize = queue.Count;
+
+                for (var i = 0; i < queueSize; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    list.Add(node.val);
+
+                    foreach (var n in node.children)
+                    {
+                        queue.Enqueue(n);
+                    }
+                }
+
+                resultList.Add(list);
+            }
+
+            return resultList;
+        }      
+    }
+}
