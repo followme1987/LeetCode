158779c418b00e6bb0ab3bb5ec86232ea636a92f
Yichen Wu <followme_1987@hotmail.com>
2019-04-16 18:07:40 +0100

    max depth of tree

---
+++ MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTreeCls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaximumDepthOfBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class MaximumDepthOfBinaryTreeCls
+    {
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            var maxLeft = MaxDepth(root.left);
+            var maxRight = MaxDepth(root.right);
+
+            return Math.Max(maxLeft, maxRight) + 1;
+        }
+    }
+}
