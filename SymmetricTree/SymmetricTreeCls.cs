1837a8265f270cb1e7fd116a54b4698cc2fe521d
Yichen Wu <followme_1987@hotmail.com>
2019-04-16 18:52:25 +0100

    symmetric Tree

---
+++ SymmetricTree/SymmetricTreeCls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SymmetricTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class SymmetricTreeCls
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            var leftTree = root.left;
+            var rightTree = root.right;
+
+            return Compare(leftTree, rightTree);
+        }
+
+        private bool Compare(TreeNode leftTree, TreeNode rightTree)
+        {
+            if (leftTree == null && rightTree == null)
+                return true;
+            if (leftTree == null || rightTree == null)
+                return false;
+
+            return leftTree.val == rightTree.val && Compare(leftTree.left, rightTree.right) && Compare(leftTree.right, rightTree.left);
+        }
+    }
+}
