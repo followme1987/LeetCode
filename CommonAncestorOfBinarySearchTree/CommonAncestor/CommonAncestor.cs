628869d08be41caf6f513946c9243be1b4fed2c0
yichen <wuyichen1987@gmail.com>
2019-12-07 19:24:59 +0000

    add questin 235

---
+++ CommonAncestorOfBinarySearchTree/CommonAncestor/CommonAncestor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CommonAncestor
+{
+    public class CommonAncestor
+    {
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root.val < p.val && root.val > q.val || root.val > p.val && root.val < q.val || root.val == q.val || root.val == p.val)
+            {
+                return root;
+            }
+            if (root.val < p.val && root.val < q.val)
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+            else
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+        }
+
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
