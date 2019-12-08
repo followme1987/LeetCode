8ff868d17e4aadcca8610ba56d8bb99035490ab5
yichen <wuyichen1987@gmail.com>
2019-12-08 11:57:25 +0000

    add question 160

---
+++ IntersectionOfTwoLinkedLists/IntersectionOfTwoLinkedLists/IntersectionOfTwoLinkedListsCls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntersectionOfTwoLinkedLists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class IntersectionOfTwoLinkedListsCls
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headB == null || headB == null)
+            {
+                return null;
+            }
+
+            ListNode currA = headA;
+            ListNode currB = headB;
+            while (currA != currB)
+            {
+                currA = currA == null ? headB : currA.next;
+                currB = currB == null ? headA : currB.next;
+            }
+            return currA;
+        }
+    }
+}
